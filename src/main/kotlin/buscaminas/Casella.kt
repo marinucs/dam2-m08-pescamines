bf19fe76dd70d0140e986e99d1bf532bbcaa225e
ferrer@windows <ismiatesting@gmail.com>
2022-10-03 00:55:59 +0200

    fets ambdues clases i tots els seus metodes, excepte comptaMines() que queda per acabar

--- src/main/kotlin/buscaminas/Casella.kt
+++ src/main/kotlin/buscaminas/Casella.kt
@@ -1,34 +1,45 @@
 package buscaminas
 
-class Casella (esMina : Boolean, estaDescoberta : Boolean, estaMarcada : Boolean, nm : Int) {
-
-    private var esMina : Boolean = false
-    private var estaDescoberta : Boolean? = null
-    private var estaMarcada : Boolean? = null
-    private var nMines : Int = 0
+class Casella (
+    private var esMina : Boolean = false,
+    private var estaDescoberta : Boolean = false,
+    private var estaMarcada : Boolean = false,
+    private var numMinesVeines : Int = 0) {
 
     fun getEsMina() = esMina
 
     fun setEsMina() {
-        esMina = true
+        if(!esMina) this.esMina = true
     }
 
     fun getDescoberta() = estaDescoberta
 
-    fun setDescoberta(estaDescoberta : Boolean) {
-        this.estaDescoberta = estaDescoberta
+    fun setDescoberta() {
+        this.estaDescoberta = true
     }
 
     fun getMinaMarcada() = estaMarcada
 
-    fun setMinaMarcada(estaMarcada : Boolean) {
-        this.estaMarcada = estaMarcada
+    fun setMarcaMina() {
+        estaMarcada = !estaMarcada
     }
 
-    fun getMines() = nMines
+    fun getNumMinesVeines() = numMinesVeines
 
-    fun setMines(nMines : Int) {
-        this.nMines = nMines
+    fun setNumMinesVeines(numMinesVeines : Int) {
+        this.numMinesVeines = numMinesVeines
+    }
+
+    override fun toString(): String {
+        var valor : String = "";
+
+        if(estaMarcada) valor = "*"
+        if(!estaDescoberta && !estaMarcada) valor = "·"
+        if(numMinesVeines == 0) valor = "0";
+        if(numMinesVeines in 1..8) valor = numMinesVeines.toString()
+        if(estaDescoberta) valor = "#"
+
+        return valor;
     }
 
 }
--- src/main/kotlin/buscaminas/Tauler.kt
+++ src/main/kotlin/buscaminas/Tauler.kt
@@ -1,4 +1,71 @@
 package buscaminas
 
-class Tauler {
+import java.lang.Math.random
+import kotlin.random.Random
+
+class Tauler(private var numCaselles : Int = 0,
+             private var numMines : Int = 0) {
+
+    private var tauler = Array(numCaselles) { Array(numCaselles) { Casella() } }
+
+    fun posaMines() {
+        for (i in 0 until numMines) {
+            var fila : Int = 0
+            var col : Int = 0
+
+            while(true) {
+                fila = Random.nextInt(0, 26)
+                col = Random.nextInt(0, 26)
+                if (!(tauler[fila][col].getEsMina())) {
+                    tauler[fila][col].setEsMina()
+                    break;
+                }
+            }
+        }
+    }
+
+    fun comptaMines() {
+        for (fila in 0 until numCaselles) {
+            for (col in 0 until numCaselles) {
+                var top = if (fila-1 >= 0) fila-1 else 0
+                var bot = if (fila+1 <= (numCaselles-1)) fila+1 else numCaselles - 1
+                var right = if (col+1 <= (numCaselles-1)) col+1 else numCaselles - 1
+                var left = if (col-1 >= 0) col-1 else 0
+            }
+        }
+    }
+
+    fun hiHaMina (fila : Int, col : Int) = tauler[fila][col].getEsMina()
+
+    fun descobert() : Boolean {
+        for (i in 0 until numCaselles) {
+            for (j in 0 until numCaselles) {
+                if (!tauler[i][j].getDescoberta() && !tauler[i][j].getEsMina()) return false
+            }
+        }
+        return true
+    }
+
+    fun descobreixCasella(fila : Int, col : Int) = tauler[fila][col].setDescoberta()
+
+    fun descobreixTauler() {
+        for (i in tauler.indices) {
+            for (j in 0 until tauler[i].size) {
+                descobreixCasella(i,j)
+            }
+        }
+    }
+
+    fun descoberta(fila : Int, col : Int)  = tauler[fila][col].getDescoberta()
+
+    fun marcaMina(fila : Int, col : Int) = tauler[fila][col].setMarcaMina()
+
+    fun minaMarcada(fila : Int, col : Int) = tauler[fila][col].getMinaMarcada()
+
+    override fun toString(): String {
+        return "";
+    }
+
 }
+
+
